1c0d29e8d44f40ee39ef75a83eb79d13059e4220
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-06-26 20:30:06 -0600

    Update Prueba dashboard.py

--- Prueba dashboard.py
+++ Prueba dashboard.py
@@ -2,8 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)

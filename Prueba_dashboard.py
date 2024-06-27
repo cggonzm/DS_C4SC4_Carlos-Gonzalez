7beef485b1ea5082242e1f3fccd1107e179263f1
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-06-26 21:11:00 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -1,20 +1,11 @@
-from vega_datasets import data as vega_data
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-movie_df = pd.read_json(vega_data.movies.url)
+titanic_data = pd.read_csv('/content/titanic.csv')
 
-def extract_year(value):
-    return pd.to_datetime(value, format='%b %d %Y').year
+selected_class = st.radio("Select Class", titanic_data['Pclass'].unique())
 
-movie_df["Year"] = movie_df["Release_Date"].apply(extract_year)
+st.write("Select Class:", selected_class)
 
-movie_2000 = movie_df[movie_df["Year"] == 2000]
-
-alt.Chart(movie_2000).mark_point(filled=True).encode(
-    alt.X('Production_Budget'),
-    alt.Y('Worldwide_Gross'),
-    alt.Size('US_Gross'),
-    alt.Color('Major_Genre'),
-    alt.OpacityValue(0.7)
-)
+selected_sex = st.selectbox("Select Sex", titanic_data['Sex'].unique())
+st.write(f"Selected Option: {selected_sex!r}")

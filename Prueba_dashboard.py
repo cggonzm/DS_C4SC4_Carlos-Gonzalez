19d4f7823e0f018d69f098b337ebeafb2da87f47
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-06-26 20:59:20 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -1,6 +1,20 @@
-import streamlit as st
+from vega_datasets import data as vega_data
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+movie_df = pd.read_json(vega_data.movies.url)
+
+def extract_year(value):
+    return pd.to_datetime(value, format='%b %d %Y').year
+
+movie_df["Year"] = movie_df["Release_Date"].apply(extract_year)
+
+movie_2000 = movie_df[movie_df["Year"] == 2000]
+
+alt.Chart(movie_2000).mark_point(filled=True).encode(
+    alt.X('Production_Budget'),
+    alt.Y('Worldwide_Gross'),
+    alt.Size('US_Gross'),
+    alt.Color('Major_Genre'),
+    alt.OpacityValue(0.7)
+)

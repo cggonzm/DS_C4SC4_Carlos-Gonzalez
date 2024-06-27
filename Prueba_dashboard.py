d68a5cc0fdea7caafe243a84cb487226a2c7609b
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-06-26 21:16:23 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-titanic_data = pd.read_csv('/content/titanic.csv')
+titanic_data = pd.read_csv(r'https://github.com/cggonzm/DS_C4SC4_Carlos-Gonzalez/blob/218cfedb4b693947478b6f9b7baed5560c1ee63b/titanic.csv')
 
 selected_class = st.radio("Select Class", titanic_data['Pclass'].unique())
 

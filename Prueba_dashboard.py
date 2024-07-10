e6e57a918ef44772cd3d9109dd5f3e92e9174a29
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-09 20:31:59 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title('DASHBOARD DESEMPEÑO SOCIALIZE YOUR KNOWLEDGE')
 
 '''
-Analisis de desempeño y datos personales de los colaboradores de socialize your knowledge_.
+Analisis de desempeño y datos personales de los colaboradores de socialize your knowledge.
 '''

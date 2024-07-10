9e4cdde6aca5ad6d4dc1a049f49fd117b787c317
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-09 20:33:13 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -4,5 +4,8 @@ from PIL import Image
 st.title('DASHBOARD DESEMPEÑO SOCIALIZE YOUR KNOWLEDGE')
 
 '''
-Analisis de desempeño y datos personales de los colaboradores de socialize your knowledge.
+Analisis de desempeño y datos personales de los colaboradores de socialize your knowledge_.
 '''
+
+image = Image.open('logo.jpg')
+st.image(image, caption='Socialize your knowledge')

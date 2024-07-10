2fec74321b129db667f23ba125889205dc046c9a
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-09 20:34:01 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -7,5 +7,5 @@ st.title('DASHBOARD DESEMPEÑO SOCIALIZE YOUR KNOWLEDGE')
 Analisis de desempeño y datos personales de los colaboradores de socialize your knowledge_.
 '''
 
-image = Image.open('logo.jpg')
+image = Image.open('https://github.com/cggonzm/DS_C4SC4_Carlos-Gonzalez/blob/main/logo.png')
 st.image(image, caption='Socialize your knowledge')

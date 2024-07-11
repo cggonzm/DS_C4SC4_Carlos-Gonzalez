503d07f2735e311d6e6d45193d6b3227105870f9
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-10 19:46:16 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -19,5 +19,5 @@ genero= st.sidebar.radio('Seleccionar genero', empleados['gender'].unique())
 
 puntaje= st.sidebar.slider('Rango de puntaje', 0, 4, (0, 4))
 
-hist= alt.Chart(empleados).mark_bar().encode(x = 'performance_score', y = 'count()')
-hist.show()
+chart= alt.Chart(empleados).mark_bar().encode(x = 'performance_score', y = 'count()')
+chart

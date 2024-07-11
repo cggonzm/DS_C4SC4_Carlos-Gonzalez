6b459d1d5d975d23150f40566a1781b4f50eb682
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-10 18:57:32 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-import numpy as np
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
+#import numpy as np
+#from bokeh.io import show, output_file
+#from bokeh.plotting import figure
 
 st.title('DASHBOARD DESEMPEÑO SOCIALIZE YOUR KNOWLEDGE')
 
@@ -22,10 +22,10 @@ puntaje= st.sidebar.slider('Rango de puntaje', 0, 4, (0, 4))
 
 genero= st.sidebar.radio('Estado civil', empleados['marital_status'].unique())
 
-hist, edges = np.histogram(employee_data['performance_score'], density=True, bins=50)
+#hist, edges = np.histogram(employee_data['performance_score'], density=True, bins=50)
 
-p = figure()
-p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color="white")
+#p = figure()
+#p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color="white")
 
-output_file("hist.html")
-show(p)
+#output_file("hist.html")
+#show(p)

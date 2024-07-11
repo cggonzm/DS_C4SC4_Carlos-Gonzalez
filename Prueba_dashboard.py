88965de2621f387f3206b47f0e0ad4110795a312
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-11 09:38:16 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-
 import altair as alt
 
 st.title('DASHBOARD DESEMPEÑO SOCIALIZE YOUR KNOWLEDGE')
@@ -17,15 +16,14 @@ empleados= pd.read_csv('Employee_data.csv')
 empleados= empleados[['name_employee', 'birth_date', 'age', 'gender', 'marital_status', 'hiring_date', 'position',
                               'salary', 'performance_score', 'last_performance_date', 'average_work_hours', 'satisfaction_level', 'absences']]
 
-genero= st.sidebar.radio('Seleccionar genero', empleados['gender'].unique())
+
+st.sidebar.radio('Seleccionar genero', empleados['gender'].unique())
 
 
-#Control para seleccionar un rango del puntaje de desempeño del empleado
+#CONTROL PARA SELECCIONAR UN RANGO DEL PUNTAJE DE DESEMPEÑO DEL EMPLEADO
 minScore = empleados["performance_score"].min()
 maxScore = empleados["performance_score"].max()
-
 mylios = st.sidebar.slider('performance_score', min_value=minScore, max_value=maxScore, value=[minScore, maxScore])
-
 empleados_filtered = empleados[(empleados["performance_score"] >= mylios[0]) & (empleados["performance_score"] <= mylios[1])].copy()
 st.write(empleados_filtered)
 

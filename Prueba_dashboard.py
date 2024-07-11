bffa4a0161a716f6d4aac35a31f0cd1e7bb84f92
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-10 20:52:21 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -14,10 +14,14 @@ image = Image.open('logo2.png')
 st.image(image, caption='Socialize your knowledge')
 
 empleados= pd.read_csv('Employee_data.csv')
+empleados= employee_data[['name_employee', 'birth_date', 'age', 'gender', 'marital_status', 'hiring_date', 'position',
+                              'salary', 'performance_score', 'last_performance_date', 'average_work_hours', 'satisfaction_level', 'absences']]
 
 genero= st.sidebar.radio('Seleccionar genero', empleados['gender'].unique())
 
 puntaje= st.sidebar.slider('Rango de puntaje', 0, 4, (0, 4))
+
+estado_civil = st.sidebar.selectbox('Estado civil:', empleados['marital_status'].unique())
 
 hist = alt.Chart(empleados).mark_bar().encode(alt.X('performance_score', bin = alt.BinParams(maxbins = 6)),
                                                   y='count()').properties(title='Distribución puntajes de desempeño')

7e46376b8d6445367e146c775c882f0d34485233
cggonzm <167793553+cggonzm@users.noreply.github.com>
2024-07-12 18:40:22 -0600

    Update Prueba_dashboard.py

--- Prueba_dashboard.py
+++ Prueba_dashboard.py
@@ -16,21 +16,19 @@ empleados= pd.read_csv('Employee_data.csv')
 empleados= empleados[['name_employee', 'birth_date', 'age', 'gender', 'marital_status', 'hiring_date', 'position',
                               'salary', 'performance_score', 'last_performance_date', 'average_work_hours', 'satisfaction_level', 'absences']]
 
-
+#CONTROL PARA SELECCIONAR EL GENERO DEL EMPLEADO
 genero= st.sidebar.radio('Seleccionar genero', empleados['gender'].unique())
-empleados_filtered = empleados[empleados['gender'] == genero]
-#st.write(empleados_filtered)
+empleados_genero = empleados[empleados['gender'] == genero]
 
 #CONTROL PARA SELECCIONAR UN RANGO DEL PUNTAJE DE DESEMPEÑO DEL EMPLEADO
-minScore = empleados_filtered["performance_score"].min()
-maxScore = empleados_filtered["performance_score"].max()
+minScore = empleados_genero["performance_score"].min()
+maxScore = empleados_genero["performance_score"].max()
 mylios = st.sidebar.slider('performance_score', min_value=minScore, max_value=maxScore, value=[minScore, maxScore])
-empleados_filtered_2 = empleados_filtered[(empleados_filtered["performance_score"] >= mylios[0]) & (empleados_filtered["performance_score"] <= mylios[1])].copy()
-st.write(empleados_filtered_2)
+empleados_performance = empleados_genero[(empleados_genero["performance_score"] >= mylios[0]) & (empleados_genero["performance_score"] <= mylios[1])].copy()
 
-
-
-estado_civil = st.sidebar.selectbox('Estado civil:', empleados['marital_status'].unique())
+#CONTROL PARA SELECCIONAR EL ESTADO CIVIL DEL EMPLEADO
+empleados_estado_civil = st.sidebar.selectbox('Estado civil:', empleados_performance['marital_status'].unique())
+st.write(empleados_estado_civil)
 
 hist = alt.Chart(empleados).mark_bar().encode(alt.X('performance_score', bin = alt.BinParams(maxbins = 6)),
                                                   y='count()').properties(title='Distribución puntajes de desempeño')
